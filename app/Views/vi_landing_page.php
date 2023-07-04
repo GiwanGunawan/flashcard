5ae6d1743c805767e8a3199408cb87ff8e52a64a
GiwanGunawan <giwangunawan@gmail.com>
2023-07-04 16:42:18 +0700

    balikin development

--- app/Views/vi_landing_page.php
+++ app/Views/vi_landing_page.php
@@ -9,7 +9,6 @@
                 <div class="row">
                     <div class="col">
 
-
                         Bismillah, atas izin Allah web/site ini diperuntukan untuk keluarga tercinta sebagai salah satu sarana belajar.
                         <br><br>
                         ^_^
@@ -22,7 +21,23 @@
                 </div>
             </div>
 
+            <footer>
+                <div class="environment">
 
+                    <p>Page rendered in {elapsed_time} seconds</p>
+
+                    <p>Environment: <?= ENVIRONMENT ?></p>
+
+                </div>
+
+                <div class="copyrights">
+
+                    <p>&copy; <?= date('Y') ?> CodeIgniter Foundation. CodeIgniter is open source project released under the MIT
+                        open source licence.</p>
+
+                </div>
+
+            </footer>
 
         </div>
 
